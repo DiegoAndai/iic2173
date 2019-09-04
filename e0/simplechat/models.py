d20976d2031352247702923f93d312e99c7914f5
DiegoAndai <dfandai@uc.cl>
2019-09-04 00:34:32 -0400

    created message model and database

--- e0/simplechat/models.py
+++ e0/simplechat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.text} by {self.author}"
